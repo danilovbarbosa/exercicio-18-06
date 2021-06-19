9b8b4f9c55816e64f5b4a07d53f8cee49fcba685
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-19 11:13:20 -0300

    env(scripts): run.sh adicionado

--- setup/settings.py
+++ setup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 
 # Application definition
@@ -77,12 +77,14 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "ENGINE": config("DB_ENGINE"),
-    "NAME": config("DB_NAME"),
-    "USER": config("DB_USER"),
-    "PASSWORD": config("DB_PASSWORD"),
-    "HOST": config("DB_HOST"),
-    "PORT": config("DB_PORT"),
+    "default": {
+        "ENGINE": config("DB_ENGINE"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
+    }
 }
 
 
