f4456aa423a12242bd139f7b0e7d391b883aaf94
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-21 11:24:34 -0300

    feat(financa/models): model Pagamento.

--- financa/urls.py
+++ financa/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 from financa import views
 
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("create/", views.create, name="create"),
+    path("read/", views.read, name="read"),
+    path("update/<int:id>", views.update, name="update"),
+    path("delete/<int:id>", views.delete, name="delete"),
+]
