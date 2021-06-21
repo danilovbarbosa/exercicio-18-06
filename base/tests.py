a3b1e6c2b14e20e3e8f8d57d10fe3d40c9bc6e89
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-21 09:08:25 -0300

    test(base/tests): test_quando_acessar_home_entao_retonar_contudo_str_home.

--- base/tests.py
+++ base/tests.py
@@ -5,8 +5,14 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_quando_acessar_home_entao_retonar_status_200_e_contudo_str_home(client):
-   url = reverse('home')
-   response = client.get(url)
-   assert response.status_code == 200
-   assert 'Home' in str(response.content)
+def test_quando_acessar_home_entao_retonar_status_200(client):
+    url = reverse("home")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_quando_acessar_home_entao_retonar_contudo_str_home(client):
+    url = reverse("home")
+    response = client.get(url)
+    assert "Home" in str(response.content)
