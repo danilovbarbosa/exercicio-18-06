84b870211ac0ee21e1f7c208c530a698add866f6
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-21 10:28:34 -0300

    feat(setup/views): estrutura das funções para CRUD criadas.

--- financa/views.py
+++ financa/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def create(request):
+    context = {}
+    return render(request, "", context=context)
+
+
+def read(request):
+    context = {}
+    return render(request, "", context=context)
+
+
+def update(request):
+    context = {}
+    return render(request, "", context=context)
+
+
+def delete(request):
+    context = {}
+    return render(request, "", context=context)
