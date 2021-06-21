f0d8cff6372e1c74c76c505f2fee71e74dac56f1
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-21 10:25:12 -0300

    feat(setup/urls): urls do app financa adicionado.

--- financa/apps.py
+++ financa/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FinancaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'financa'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "financa"
