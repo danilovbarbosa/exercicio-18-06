c135cfc8690aec803795a40969b0c2199136012d
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-21 09:14:15 -0300

    test(base/tests): test_quando_acessar_url_home_entao_retonar_template_home.

--- base/tests.py
+++ base/tests.py
@@ -16,3 +16,12 @@ def test_quando_acessar_home_entao_retonar_contudo_str_home(client):
     url = reverse("home")
     response = client.get(url)
     assert "Home" in str(response.content)
+
+
+class TestPageHome(TestCase):
+    def test_quando_acessar_url_home_entao_retonar_template_home(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "base.html")
+        self.assertContains(response, "Home")
