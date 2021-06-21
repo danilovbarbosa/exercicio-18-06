1936a794500077fc609f293d684a9b9bb9e92ead
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-21 11:29:00 -0300

    feat(financa/forms): form Pagamento.

--- financa/forms.py
+++ financa/forms.py
@@ -1,1 +1,8 @@
-# from django import models.forms
+from django.forms import ModelForm
+from financa.models import Pagamento
+
+
+class PagamentoForm(ModelForm):
+    class Meta:
+        model = Pagamento
+        fields = "__all__"
