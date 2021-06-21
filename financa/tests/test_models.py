83ff493c67572418c4cfe474df261a548fecb908
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-21 16:56:22 -0300

    test(financa/test_models): teste para formulario de pagamento.

--- financa/tests/test_models.py
+++ financa/tests/test_models.py
@@ -1,6 +1,20 @@
 import pytest
 from django.urls import reverse
 
+from financa.models import Pagamento
+from financa.forms import PagamentoForm
+
+
+@pytest.fixture
+def obj_pagamento():
+    pagamento = Pagamento.objects.create(
+        id_pedido=1,
+        id_vendedor=1,
+        status_pedido="FA",
+        valor_pedido=1,
+        valor_delivery=1,
+    )
+    return pagamento
 
 
 @pytest.mark.django_db
@@ -30,16 +44,17 @@ def test_quando_acessar_read_entao_retonar_contudo_str_home(client):
     response = client.get(url)
     assert "Lista de pagamentos" in str(response.content)
 
-# TODO: Finalizar.
-# @pytest.mark.django_db
-# def test_quando_acessar_update_entao_retonar_status_200(client):
-#     url = reverse("update", kwargs={'id':'0'})
-#     response = client.get(url)
-#     assert response.status_code == 200
 
-
-# @pytest.mark.django_db
-# def test_quando_acessar_update_entao_retonar_contudo_str_home(client):
-#     url = reverse("read")
-#     response = client.get(url)
-#     assert "Lista de pagamentos" in str(response.content)
+@pytest.mark.django_db
+def test_valid_form(obj_pagamento):
+    data = {
+        "id_pedido": obj_pagamento.id_pedido,
+        "id_vendedor": obj_pagamento.id_vendedor,
+        "status_pedido": obj_pagamento.status_pedido,
+        "valor_pedido": obj_pagamento.valor_pedido,
+        "valor_delivery": obj_pagamento.valor_delivery,
+    }
+    
+    form = PagamentoForm(data=data)
+    
+    assert form.is_valid() == True
