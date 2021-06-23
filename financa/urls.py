c0323785c43c2db05b5f9fb06fa9198b617bfe6a
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-23 11:05:51 -0300

    feat(financa/urls): adicioanda routers.

--- financa/urls.py
+++ financa/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
 
 from financa import views
 
+router = routers.DefaultRouter()
+router.register(r"pagamentos", views.PagamentoViewSet)
+
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("create/", views.create, name="create"),
     path("read/", views.read, name="read"),
     path("update/<int:id>", views.update, name="update"),
